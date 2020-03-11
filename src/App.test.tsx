54791a1d3d8746a9139e7dfe1a46f8f938606a44
shenphen <shenphen@protonmail.com>
2020-03-11 22:07:51 +0100

    Setup Enzyme & remove @testing-library dependencies

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { shallow } from 'enzyme'
 import App from './App'
+import User from 'components/User'
 
-test('renders learn react link', () => {
-    const { getByText } = render(<App />)
-    const linkElement = getByText(/learn react/i)
-    expect(linkElement).toBeInTheDocument()
+describe('App', () => {
+    const $ = shallow(<App />)
+
+    it('should render header', () => {
+        const header = $.find('.App-header')
+        expect(header).toHaveLength(1)
+    })
+
+    it('should render User component', () => {
+        const user = $.find(User)
+        expect(user).toHaveLength(1)
+    })
 })
---
+++ src/components/User/User.tsx
@@ -0,0 +1,7 @@
+import React, { Fragment } from 'react'
+
+function User() {
+    return <Fragment />
+}
+
+export default User
